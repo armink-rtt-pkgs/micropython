8860e183f5ad68f6b62cd92c5d282bcfb129443c
SummerGift <zy_770077@qq.com>
2018-01-05 15:46:21 +0800

    update lib/oofatfs/ff.h

--- lib/oofatfs/ff.h
+++ lib/oofatfs/ff.h
@@ -50,6 +50,8 @@ typedef uint32_t DWORD;
 /* This type MUST be 64-bit (Remove this for C89 compatibility) */
 typedef uint64_t QWORD;
 
+#define FFCONF_H "lib/oofatfs/ffconf.h"
+
 #include FFCONF_H          /* FatFs configuration options */
 
 #if _FATFS != _FFCONF
