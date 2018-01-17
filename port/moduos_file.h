3b1c4b44adfcc11127ecb1839272c33e3f6aedd1
SummerGift <zy_770077@qq.com>
2018-01-17 17:02:36 +0800

    update License

--- port/moduos_file.h
+++ port/moduos_file.h
@@ -3,7 +3,7 @@
  *
  * The MIT License (MIT)
  *
- * Copyright (c) 2017 Damien P. George
+ * Copyright (c) 2017 SummerGift <zhangyuan@rt-thread.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
