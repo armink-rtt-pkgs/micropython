bfaae6b6d4e5fd3782da14173dbb69e479f04345
SummerGift <zy_770077@qq.com>
2018-01-17 16:56:41 +0800

    update moduos.c

--- port/moduos.c
+++ port/moduos.c
@@ -131,4 +131,8 @@ STATIC const mp_rom_map_elem_t os_module_globals_table[] = {
 
 STATIC MP_DEFINE_CONST_DICT(os_module_globals, os_module_globals_table);
 
-const mp_obj_module_t mp_module_uos = { .base = { &mp_type_module }, .globals = (mp_obj_dict_t*) &os_module_globals, };
+const mp_obj_module_t mp_module_uos = {
+        .base = { &mp_type_module },
+        .globals = (mp_obj_dict_t*) &os_module_globals,
+};
+
