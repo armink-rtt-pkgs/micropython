c6dcc02fb3a995fccd4b702642557305f130bf43
armink <armink.ztl@gmail.com>
2018-04-27 18:16:01 +0800

    【完善】串口注释。
    Signed-off-by: armink <armink.ztl@gmail.com>

--- port/machine_uart.c
+++ port/machine_uart.c
@@ -218,12 +218,16 @@ STATIC MP_DEFINE_CONST_DICT(machine_uart_locals_dict, machine_uart_locals_dict_t
 STATIC mp_uint_t machine_uart_read(mp_obj_t self_in, void *buf_in, mp_uint_t size, int *errcode) {
     machine_uart_obj_t *self = self_in;
     byte *buf = buf_in;
+    //TODO dfs sync read
+    MP_RTT_NOT_IMPL_PRINT;
     return rt_device_read((struct rt_device *)(self->uart_device), -1, buf, size);
 }
 
 STATIC mp_uint_t machine_uart_write(mp_obj_t self_in, const void *buf_in, mp_uint_t size, int *errcode) {
     machine_uart_obj_t *self = self_in;
     const byte *buf = buf_in;
+    //TODO dfs sync write
+    MP_RTT_NOT_IMPL_PRINT;
     return rt_device_write((struct rt_device *)(self->uart_device), -1, buf, size);
 }
 
