6cd2d1ff629c934fd3b080ac3447cdac934cbfd1
Bernard Xiong <bernard.xiong@gmail.com>
2019-01-26 12:44:36 +0800

    Include rthw.h header file to fix compiling issue in SMP

--- port/mpconfigport.h
+++ port/mpconfigport.h
@@ -23,7 +23,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-
+#include <rthw.h>
 #include <rtthread.h>
 #include <stdint.h>
 #include <stdio.h>
